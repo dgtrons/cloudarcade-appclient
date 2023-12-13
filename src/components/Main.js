e86cfdb5d821c5cf8248f5726ff398f535b5ddc1
Diego Reyes <diego.reyes.t@ug.uchile.cl>
2023-12-13 12:57:03 -0300

    Added styles to Main and App js files

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Main.css";
 
 export default function Main() {
+
+  const inputRef = useRef(null)
+
   return (
-    <div>
-      <h1>React Router DOM v6</h1>
-      <p>React Router DOM v6 is here!</p>
+    <div className="container">
+      <h1 className="title text-light">AZ900 training quiz</h1>
+
+      <ol>
+        <li>Answer the following mutiple-choice questions</li>
+        <li>You will receive a score report at the end</li>
+      </ol>
+
+      <form id="form">
+        <input ref={inputRef} className="userid" type="text" placeholder="Username" />
+      </form>
+
+      <div className="start">
+        <Link to={'quiz'} className="btn">Start Quiz</Link>
+      </div>
     </div>
   )
 }   
