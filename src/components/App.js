af6661c6296586147836c5720fb02ab929e8f026
Diego Reyes <diego.reyes.t@ug.uchile.cl>
2023-12-13 11:54:38 -0300

    restructuration of folder and solving references

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Cloud Arcade
+      </header>
+    </div>
+  );
+}
+
+export default App;
