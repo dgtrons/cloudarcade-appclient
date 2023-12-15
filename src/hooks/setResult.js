a4695360811ae2bc29c82348460b6c9c98a05e7c
Diego Reyes <diego.reyes.t@ug.uchile.cl>
2023-12-15 19:00:01 -0300

    added the result action to pass the id of the option when clicking next, some inconsitencies in the logic will be solved later

---
+++ src/hooks/setResult.js
@@ -0,0 +1,10 @@
+import * as ActionTypes from '../redux/result_reducer';
+
+export const PushAnswer = (result) => async(dispatch) => {
+    try {
+        /** dispatch an action */
+        await dispatch(Action.pushResultAction(result));
+    } catch (error) {
+        console.log(error)
+    }   
+}
